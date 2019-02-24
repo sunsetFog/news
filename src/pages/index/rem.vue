19a1ada2a251709ead0d019434a0b61083c539dd
mimikiss <1456300078@qq.com>
2019-02-24 15:02:54 +0800

    setting

--- src/pages/index/rem.vue
+++ src/pages/index/rem.vue
@@ -1,5 +1,7 @@
 <template>
     <section>
+        <!-- 注意-注意:谷歌浏览器最小字体为12像素，但手机上没限制
+        因此，想用10字体像素，得谷歌设置-自定义字体为10以下 -->
         <!-- rem设置看本项目index.html -->
         <div class="adapt">移动端px转rem</div>
     </section>
@@ -13,20 +15,24 @@ export default {
 
 <style>
 /* 375的设计图量像素除以16 */
-/* 345/16=21.5625
-    40/16=2.5
-    15/16=0.9375 
+/* 345/10=34.5
+    40/10=4
+    15/10=1.5 
     margin得转rem,除了百分比，横屏的都要自适应
 */
     .adapt{
-        width: 21.5625rem;
-        height: 2.5rem;
-        margin-left: 0.9375rem;
+        width: 34.5rem;
+        height: 4rem;
+        margin-left: 1.5rem;
         background: plum;
         text-align: center;
-        line-height: 2.5rem;
+        line-height: 4rem;
         color: white;
+        font-size: 1.6rem;
+        border-radius: 4px;
+        border: 1px solid red;
     }
+    /* 移动端像素都要转rem */
 </style>
 
 
