69025633899054604f4c5d90a8d6588069aaeae7
unknown <you@example.com>
2020-06-23 23:06:02 +0800

    breezy-9499

--- src/pages/managementCenter/knowledge/mock.vue
+++ src/pages/managementCenter/knowledge/mock.vue
@@ -1,105 +1,109 @@
 <template>
-<section id="mock">
-<!--mock本地模拟数据-->
-    <el-input v-model="obj.id" placeholder="请输入id" style="width: 200px" />
-    <el-input v-model="obj.name" placeholder="请输入name" style="width: 200px" />
-    <el-input v-model="obj.content" placeholder="请输入content" style="width: 200px" />
-    <el-button type="primary" @click="addList">添加</el-button>
-    <el-button type="warning" @click="updateList">修改</el-button>
-    <ul>
-      <li v-for="item in list">
-        id={{item.id}}
-        //name=:{{item.name}}
-        //content={{item.content}}
-        <el-button @click="deleteList(item.id)">删除</el-button>
-      </li>
-    </ul>
-</section>
+    <section id="mock">
+      <!--mock本地模拟数据-->
+    </section>
 </template>
 
 <script>
 //npm install mockjs --save
-import mock from '../../../../static/study/mock.js';
-	export default{
-		data(){
-			return{
-				list: [],
-				obj: {  //将添加的数据存到obj对象中
-          id: '',
-          name: '',
-          content: ''
-				}
-			}
-		},
-		created(){
-      //查询列表数据
-      this.getData();
-      this.getJson();
-		},
-		 methods:{
-       getJson(){
-          //1.开发环境模拟本地数据    请查看see/build/webpack.dev.conf.js  这没有实用性，删除了
-          //2.mock模拟本地数据
-          this.$axios.post('http://text.com').then(function(res){
-              console.log('moon',res);
-          });
-       },
-			// 查询列表数据
-      getData(){
-        this.$axios.get('/list').then(res => {
-          this.list = res.data.data;
-          console.log('查询数据',this.list);
-        });
-      },
+import mock from "../../../../static/study/breezy/mock.js";
+export default {
+  data() {
+    return {
+      list: []
+    };
+  },
+  created() {
+    // this.getJson();
+    // this.is_delete();
+  },
+  methods: {
+    is_delete(){
 
-      // 删除列表数据
-      deleteList(id){
-        this.$axios.post('/list',{
-          params: {
-           id: id
-          }
-        }).then(res => {
-            this.list = res.data.data;
-        });
-      },
+        let json = {goods_id: 3};
+        this.$axios.post("plugin.store-cashier.frontend.store.goods.destroy-goods",json).then(function(res) {
+            console.log("delete++=", res);
+        }).catch((err)=>{
+            console.error('error',err);
+        })
+      
+    },
+    getJson() {
+      //1.开发环境模拟本地数据    请查看see/build/webpack.dev.conf.js  这没有实用性，删除了
+      //2.mock模拟本地数据
+      //查找全部
+      this.$axios.post('plugin.store-cashier.frontend.store.goods.get-goods-list').then(function(res){
+          console.log('moon++=',res);
+      }).catch((err)=>{
+          console.error('error',err);
+      })
 
-      // 增加列表数据
-      addList(){
-        this.$axios.post('/listAdd',{
-          params: {
-            obj: this.obj
-          }
-        }).then(res => {
-             console.log("请求成功"+res.data.data);
-            this.list = res.data.data;
-        });
-      },
+      //查找单条
+      // let json = {
+      //   goods_id: 3 //商品id
+      // };
+      // this.$axios.post("plugin.store-cashier.frontend.store.goods.get-goods-detail",json).then(function(res) {
+      //     console.log("single++=", res);
+      // }).catch((err)=>{
+      //   console.error('error',err);
+      // })
 
-      // 修改列表数据
-      updateList(){
-        console.log('修改',this.obj);
-        var that = this;
-        this.$axios.post('/listUpdate',{
-          params: {
-            obj: this.obj
-          }
-        }).then(function(res){
-          that.list = res.data.data;
-          console.log("请求的数据",that.list);
-        });
-      }
+      //查找分类
+      // let json = {store_id: 12};
+      // this.$axios.post('plugin.store-cashier.frontend.store.goods-category.get-category-by-store-id',json).then(function(res){
+      //     console.log('cate++=',res);
+      // }).catch((err)=>{
+      //     console.error('error',err);
+      // })
 
-		 }
-	}
+
+      //修改
+      // let json = {
+      //                       store_id: 12,//店铺id
+      //                       id: 3,//商品id
+      //                       title: '1',//商品名称
+      //                       company: '1',//商品单位
+      //                       category_pid: '1',//商品分类一级id
+      //                       category_cid: '1',//商品分类二级id
+      //                       thumb: '../../../../static/meitu/dian/lizhi.jpg',//商品图片
+      //                       price: '1',//商品现价
+      //                       original_price: '1',//商品原价
+      //                       cost: '1',//商品成本
+      //                       stock: '1',//库存
+      //                       virtual_sales: '1'//虚拟销量
+      //                   };
+      // this.$axios.post('plugin.store-cashier.frontend.store.goods.edit-goods',json).then(function(res){
+      //     console.log('cate++=',res);
+      // }).catch((err)=>{
+      //     console.error('error',err);
+      // })
+
+
+      //添加
+        //     let json = {
+        //                     store_id: 12,//店铺id
+        //                     title: '2',//商品名称
+        //                     company: '2',//商品单位
+        //                     category_pid: '2',//商品分类一级id
+        //                     category_cid: '2',//商品分类二级id
+        //                     thumb: '../../../../static/meitu/dian/lizhi.jpg',//商品图片
+        //                     price: '2',//商品现价
+        //                     original_price: '2',//商品原价
+        //                     cost: '2',//商品成本
+        //                     stock: '2',//库存
+        //                     virtual_sales: '2'//虚拟销量
+        //                 };
+        // this.$axios.post('plugin.store-cashier.frontend.store.goods.add-goods',json).then(function(res){
+        //     console.log('cate++=',res);
+        // }).catch((err)=>{
+        //     console.error('error',err);
+        // })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
-#mock{
-  ul{
-    li{
-      .mixin_li(100%,50px);
-      color: @color_blueviolet;
-    }
-  }
+#mock {
 }
 </style>
---
+++ static/study/breezy/lianxi.vue
@@ -0,0 +1,197 @@
+<template>
+    <section id="lianxi">
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: "移动端练习",
+    data(){
+        return{
+
+        }
+    },
+    created(){
+        // 前端关于自己模拟接口做测试
+        // https://blog.csdn.net/w_s_x_b/article/details/92613421
+        // https://www.mocky.io/  这是接口模拟工具  https://designer.mocky.io
+    },
+    mounted(){
+
+    },
+    methods:{
+        //商品管理-商品数据获取
+        getJson(){
+            let that = this;
+            let json = {store_id: 12};//店铺id
+            that.$http.post('plugin.store-cashier.frontend.store.goods.get-goods-list',json).then((res) => {
+
+                let res2={
+                    count: 3,//商品数量
+                    list:   [
+                                {
+                                    store_id: 12,//店铺id
+                                    id: 1,//商品id
+                                    title: '5斤装桂味荔枝现摘现发',//商品名称
+                                    company: '包',//商品单位
+                                    category_pid: 201,//商品分类一级id
+                                    category_cid: 202,//商品分类二级id
+                                    thumb: '../../../../static/meitu/dian/lizhi.jpg',//商品图片
+                                    price: 110,//商品现价
+                                    original_price: 160,//商品原价
+                                    cost: 25,//商品成本
+                                    stock: 1000,//库存
+                                    virtual_sales: 2300//虚拟销量    
+                                },
+                                {
+                                    store_id: 12,
+                                    id: 2,
+                                    title: '海南金煌新鲜芒果',
+                                    company: '箱',
+                                    category_pid: 301,
+                                    category_cid: 302,
+                                    thumb: '../../../../static/meitu/dian/mangguo.jpg',
+                                    price: 120,
+                                    original_price: 180,
+                                    cost: 20,
+                                    stock: 1500,
+                                    virtual_sales: 3500
+                                },
+                                {
+                                    store_id: 12,
+                                    id: 3,
+                                    title: '新鲜采摘龙眼甜桂圆肉',
+                                    company: '袋',
+                                    category_pid: 401,
+                                    category_cid: 402,
+                                    thumb: '../../../../static/meitu/dian/longyan.jpg',
+                                    price: 140,
+                                    original_price: 220,
+                                    cost: 35,
+                                    stock: 800,
+                                    virtual_sales: 4700
+                                }
+                            ]
+                    }
+
+            }).catch((err)=>{
+                console.error('error',err);
+            })
+        },
+        //商品管理-商品删除
+        commodityDelete(){
+            let that = this;
+            let json = {
+                            goods_id: ''//商品id
+                        };
+            that.$http.post('plugin.store-cashier.frontend.store.goods.destroy-goods',json).then((res) => {
+
+                let res2 = {result: 1}//1是成功状态，其他失败状态
+
+            }).catch((err)=>{
+                console.error('error',err);
+            })
+        },
+        //商品分类
+        category(){
+            let that = this;
+            let json = {store_id: 12};//店铺id
+            that.$http.post('plugin.store-cashier.frontend.store.goods-category.get-category-by-store-id',json).then((res) => {
+                let res2 = [
+                                {   
+                                    id: 'shipin',
+                                    label: '食品',
+                                    children: [{id: 'yinpin',label: '饮品'},{id: 'shuiguo',label: '水果'},{id: 'lingshi',label: '零食'},{id: 'cha',label: '茶'}]
+                                },
+                                {   
+                                    id: 'shengxian',
+                                    label: '生鲜',
+                                    children: [{id: 'roulei',label: '肉类'},{id: 'shuichan',label: '水产'},{id: 'liaoli',label: '料理'},{id: 'shucai',label: '蔬菜'}]
+                                },
+                                {   
+                                    id: 'jiazhuang',
+                                    label: '家装',
+                                    children: [{id: 'jiaju',label: '家具'},{id: 'wujin',label: '五金'},{id: 'jiancai',label: '建材'}]
+                                }
+                            ]
+
+
+            }).catch((err)=>{
+                console.error('error',err);
+            })
+        },
+        //上传图片接口
+        upload(){
+            //https://jsonplaceholder.typicode.com/posts/
+        },
+        //发布商品
+        publish(){
+            let that = this;
+            let json = {
+                            store_id: 12,//店铺id
+                            title: '',//商品名称
+                            company: '',//商品单位
+                            category_pid: '',//商品分类一级id
+                            category_cid: '',//商品分类二级id
+                            thumb: '../../../../static/meitu/dian/lizhi.jpg',//商品图片
+                            price: '',//商品现价
+                            original_price: '',//商品原价
+                            cost: '',//商品成本
+                            stock: '',//库存
+                            virtual_sales: ''//虚拟销量
+                        };
+            that.$http.post('plugin.store-cashier.frontend.store.goods.add-goods',json).then((res) => {
+                let res2 = {result: 1}//1是成功状态，其他失败状态
+
+
+            }).catch((err)=>{
+                console.error('error',err);
+            })
+        },
+        //获取单条商品数据
+        getGoodsJson(){
+            let that = this;
+            let json = {
+                            goods_id: ''//商品id
+                        };
+            that.$http.post('plugin.store-cashier.frontend.store.goods.get-goods-detail',json).then((res) => {
+
+
+
+            }).catch((err)=>{
+                console.error('error',err);
+            })
+        },
+        //确认编辑
+        editor(){
+            let that = this;
+            let json = {
+                            store_id: 12,//店铺id
+                            id: '',//商品id
+                            title: '',//商品名称
+                            company: '',//商品单位
+                            category_pid: '',//商品分类一级id
+                            category_cid: '',//商品分类二级id
+                            thumb: '../../../../static/meitu/dian/lizhi.jpg',//商品图片
+                            price: '',//商品现价
+                            original_price: '',//商品原价
+                            cost: '',//商品成本
+                            stock: '',//库存
+                            virtual_sales: ''//虚拟销量
+                        };
+            that.$http.post('plugin.store-cashier.frontend.store.goods.edit-goods',json).then((res) => {
+                let res2 = {result: 1}//1是成功状态，其他失败状态
+
+
+            }).catch((err)=>{
+                console.error('error',err);
+            })
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
