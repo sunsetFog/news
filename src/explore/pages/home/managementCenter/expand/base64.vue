dc9007b36b6cdc15093249d47ae3d7578fd431b8
ivory <jsivory@itcom666.com>
2023-07-19 21:59:59 +0800

    2023-07-19 21:59

--- src/explore/pages/home/managementCenter/expand/base64.vue
+++ src/explore/pages/home/managementCenter/expand/base64.vue
@@ -1,74 +1,32 @@
 <template>
     <section id="base64">
-        <!-- base64解密 -->
         <!-- Base64加密解密站长工具  http://tool.chinaz.com/Tools/Base64.aspx -->
         <ul>
             <li v-for="item in encryption">
-                {{b64Decode(item.title)}}
+                ------原来: {{item.title1}}------加密: {{item.title2}}------解密: {{decodeWay(item.title2)}}
             </li>
         </ul>
     </section>
 </template>
 
 <script>
+const Base64 = require("js-base64").Base64
 export default {
     data(){
         return{
-            encryption: [{title: 'TGVzbGll'},{title: '5bCP5b2s'},{title: '5LqM5Lik'},{title: 'MTIzNDU2'}],
+            encryption: [{title1: '彤彤'},{title1: '小彬'},{title1: '里克'},{title1: '123456'}],
+        }
+    },
+    created() {
+        for (let i = 0; i < this.encryption.length; i++) {
+            let item = this.encryption[i];
+            item.title2 = Base64.encode(item.title1);// base64加密
         }
     },
     methods: {
-        b64Decode(input){
-            var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-            var output = "";
-            var chr1, chr2, chr3;
-            var enc1, enc2, enc3, enc4;
-            var i = 0;
-            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-            while (i < input.length) {
-                enc1 = _keyStr.indexOf(input.charAt(i++));
-                enc2 = _keyStr.indexOf(input.charAt(i++));
-                enc3 = _keyStr.indexOf(input.charAt(i++));
-                enc4 = _keyStr.indexOf(input.charAt(i++));
-                chr1 = (enc1 << 2) | (enc2 >> 4);
-                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                chr3 = ((enc3 & 3) << 6) | enc4;
-                output = output + String.fromCharCode(chr1);
-                if (enc3 != 64) {
-                    output = output + String.fromCharCode(chr2);
-                }
-                if (enc4 != 64) {
-                    output = output + String.fromCharCode(chr3);
-                }
-            }
-            output = this.utf8_decode(output);
-            return output;
-    
-        },
-        utf8_decode (utftext) { 
-            var string = "";
-            var i = 0;
-            //var c = c1 = c2 = 0;
-            var c,c1,c2,c3;
-
-            while ( i < utftext.length ) {
-                c = utftext.charCodeAt(i);
-                if (c < 128) {
-                    string += String.fromCharCode(c);
-                    i++;
-                } else if((c > 191) && (c < 224)) {
-                    c2 = utftext.charCodeAt(i+1);
-                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                    i += 2;
-                } else {
-                    c2 = utftext.charCodeAt(i+1);
-                    c3 = utftext.charCodeAt(i+2);
-                    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                    i += 3;
-                }
-            }
-            return string;
-        },
+        decodeWay(value) {
+            return Base64.decode(value);// // base64解密
+        }
     }
 }
 </script>
